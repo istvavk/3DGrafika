4a3ce5f49ff1a7dc7288689a25453298d5faca13
Antonio Ištvanović <87823353+istvavk@users.noreply.github.com>
2023-01-07 04:40:44 +0100

    Add files via upload

---
+++ zadaca3/prvi&drugi/rasterizer.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include "tgaimage.h"
+using namespace std;
+
+// dimenzije slike
+const int width  = 512;
+const int height = 512;
+
+// definiramo boje
+const TGAColor green = TGAColor(0, 255, 0, 1);
+const TGAColor red   = TGAColor(255, 0, 0, 1);
+const TGAColor blue  = TGAColor(0, 0, 255, 1);
+
+void draw_triangle_2d(TGAImage& image, int x0, int y0, int x1, int y1, int x2, int y2, TGAColor color) {
+    // Poredaj vrhove uzlaznim redoslijedom y koordinate
+    if (y0 > y1) {
+        swap(y0, y1);
+        swap(x0, x1);
+    }
+    if (y1 > y2) {
+        swap(y2, y1);
+        swap(x2, x1);
+    }
+    if (y0 > y1) {
+        swap(y0, y1);
+        swap(x0, x1);
+    }
+
+    // racuna nagibe dviju stranica trokuta
+    float slope1 = (x1 - x0) / (float)(y1 - y0);
+    float slope2 = (x2 - x0) / (float)(y2 - y0);
+
+    // iteriramo kroz redove
+    for (int y = y0; y <= y1; y++) {
+        // racuna x koordinate dvaju rubova trokuta za ovaj redak
+        int xl = x0 + slope1 * (y - y0);
+        int xr = x0 + slope2 * (y - y0);
+
+        // popunjavamo piksele između dva ruba bojom
+        for (int x = xl; x <= xr; x++) {
+            image.set(x, y, color);
+        }
+    }
+
+    // racuna nagibe dviju stranica trokuta
+    slope1 = (x2 - x1) / (float)(y2 - y1);
+    slope2 = (x2 - x0) / (float)(y2 - y0);
+
+    // iteriramo kroz redove
+    for (int y = y1; y <= y2; y++) {
+        // racuna x koordinate dvaju rubova trokuta za ovaj redak
+        int xl = x1 + slope1 * (y - y1);
+        int xr = x0 + slope2 * (y - y0);
+
+        // popunjavamo piksele između dva ruba bojom
+        for (int x = xl; x <= xr; x++) {
+            image.set(x, y, color);
+        }
+    }
+}
+
+void draw_triangle_2d_gouraud(TGAImage& image, int x0, int y0, int x1, int y1, int x2, int y2,
+                              TGAColor c0, TGAColor c1, TGAColor c2) {
+    // poredaj vrhove uzlaznim redoslijedom y koordinate
+    if (y0 > y1) {
+        swap(y0, y1);
+        swap(x0, x1);
+        swap(c0, c1);
+    }
+    if (y1 > y2) {
+        swap(y2, y1);
+        swap(x2, x1);
+        swap(c2, c1);
+    }
+    if (y0 > y1) {
+        swap(y0, y1);
+        swap(x0, x1);
+        swap(c0, c1);
+    }
+
+    // racuna nagibe dviju stranica trokuta
+    float slope1 = (x1 - x0) / (float)(y1 - y0);
+    float slope2 = (x2 - x0) / (float)(y2 - y0);
+
+    // interpoliramo boje duzinom prve polovice trokuta
+    for (int y = y0; y <= y1; y++) {
+        // racuna x koordinate dvaju rubova trokuta za ovaj redak
+        int xl = x0 + slope1 * (y - y0);
+        int xr = x0 + slope2 * (y - y0);
+
+        // interpoliramo boje između dva ruba
+        float s = (y - y0) / (float)(y1 - y0);
+        TGAColor c = c0 * (1 - s) + c1 * s;
+
+        // ispunjava piksele između dva ruba interpoliranom bojom
+        for (int x = xl; x <= xr; x++) {
+            image.set(x, y, c);
+        }
+    }
+
+    // racuna nagibe dviju stranica trokuta
+    slope1 = (x2 - x1) / (float)(y2 - y1);
+    slope2 = (x2 - x0) / (float)(y2 - y0);
+
+    // interpoliramo boje duzinom druge polovice trokuta
+    for (int y = y1; y <= y2; y++) {
+        // racuna x koordinate dvaju rubova trokuta za ovaj redak
+        int xl = x1 + slope1 * (y - y1);
+        int xr = x0 + slope2 * (y - y0);
+
+        // interpoliramo boje između dva ruba
+        float s = (y - y1) / (float)(y2 - y1);
+        TGAColor c = c1 * (1 - s) + c2 * s;
+
+        // ispunjava piksele između dva ruba interpoliranom bojom
+        for (int x = xl; x <= xr; x++) {
+            image.set(x, y, c);
+        }
+    }
+}
+
+int main() {
+    // Napravi 512x512 sliku
+    TGAImage image(width, height, TGAImage::RGB);
+
+    // Nacrtaj zeleni trokut
+    draw_triangle_2d(image, 10, 10, 50, 50, 100, 10, green);
+
+    // Nacrtaj crveni trokut
+    draw_triangle_2d(image, 200, 200, 250, 250, 300, 200, red);
+
+    // Draw a red triangle with Gouraud shading
+    draw_triangle_2d_gouraud(image, 200, 200, 250, 250, 300, 200, red, blue, green);
+
+    // spremi sliku
+    image.flip_vertically();
+    image.write_tga_file("triangle.tga");
+}
---
+++ zadaca3/treci&cetvrti/rasterizer.cpp
@@ -0,0 +1,178 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include "tgaimage.h"
+using namespace std;
+
+
+typedef float Vec3[3];
+typedef float Vec2[2];
+
+// dimenzije slike
+const int width  = 512;
+const int height = 512;
+
+// definiramo boje
+const TGAColor green = TGAColor(0, 255, 0, 1);
+const TGAColor red   = TGAColor(255, 0, 0, 1);
+const TGAColor blue  = TGAColor(0, 0, 255, 1);
+
+
+float rubnafunk(const Vec3 &a, const Vec3 &b, const Vec3 &c) {
+    return (c[0] - a[0]) * (b[1] - a[1]) - (c[1] - a[1]) * (b[0] - a[0]);
+}
+
+//funkcija iscrtava trokut kroz zadane pixele
+void draw_triangle(float x0, float y0,float z0, float x1, float y1,float z1, float x2, float y2,float z2 ,TGAImage &image,TGAColor color)
+{
+    Vec3 a = {x0,y0,z0};
+    Vec3 b = {x1,y1,z1};
+    Vec3 c = {x2, y2,z2};
+
+    // dijeli koordinate sa z
+    a[0]/=a[2]; a[1]/=a[2];
+    b[0]/=b[2]; b[1]/=b[2];
+    c[0]/=c[2]; c[1]/=c[2];
+
+    //tranformacija
+    a[0] = (1 + a[0]) * 0.5 * width; a[1]= (1 + a[1]) * 0.5 * height;
+    b[0] = (1 + b[0]) * 0.5 * width; b[1]= (1 + b[1]) * 0.5 * height;
+    c[0] = (1 + c[0]) * 0.5 * width; c[1]= (1 + c[1]) * 0.5 * height;
+
+    // 1/z
+    a[2] = 1 /a[2];  b[2]= 1 /b[2]; c[2] = 1 /c[2];
+
+    for (int j =0; j<height ; j++){
+        for (int i= 0;i<width;i++){
+            Vec3 p = {((float)i+0.5f),(height-(float)j+0.5f),0};
+            float alfa = rubnafunk(c,b,p);
+            float beta =rubnafunk(a,c,p);
+            float gama = rubnafunk(b,a,p);
+            if((alfa >= 0 && beta >= 0 && gama >= 0) ) {
+                image.set(j,i,color);
+
+            }
+        }
+    }
+}
+
+void draw_triangle_tex(float x0, float y0, float z0, float u0, float v0,
+                       float x1, float y1, float z1, float u1, float v1,
+                       float x2, float y2, float z2, float u2, float v2,
+                       const char* file_path, TGAImage& image) {
+    // Učitava teksturu s file patha
+    TGAImage texture;
+    texture.read_tga_file(file_path);
+
+    Vec3 a = {x0, y0, z0};
+    Vec3 b = {x1, y1, z1};
+    Vec3 c = {x2, y2, z2};
+    Vec2 uv_a = {u0, v0};
+    Vec2 uv_b = {u1, v1};
+    Vec2 uv_c = {u2, v2};
+
+    // Dijeli koordinate sa z
+    a[0] /= a[2]; a[1] /= a[2];
+    b[0] /= b[2]; b[1] /= b[2];
+    c[0] /= c[2]; c[1] /= c[2];
+
+    // transformacija koordinata
+    a[0] = (1 + a[0]) * 0.5 * width; a[1] = (1 + a[1]) * 0.5 * height;
+    b[0] = (1 + b[0]) * 0.5 * width; b[1] = (1 + b[1]) * 0.5 * height;
+    c[0] = (1 + c[0]) * 0.5 * width; c[1] = (1 + c[1]) * 0.5 * height;
+
+    // inverz od 1/z
+    a[2] = 1 / a[2]; b[2] = 1 / b[2]; c[2] = 1 / c[2];
+
+    for (int j = 0; j < height; j++) {
+        for (int i = 0; i < width; i++) {
+            Vec3 p = {(float)i + 0.5f, height - (float)j + 0.5f, 0};
+            float alpha = rubnafunk(c, b, p);
+            float beta = rubnafunk(a, c, p);
+            float gama = rubnafunk(b, a, p);
+            if (alpha >= 0 && beta >= 0 && gama >= 0) {
+                // Interpolira UV koordinate pomoću baricentričnih koordinata
+                float w = alpha / (alpha + beta + gama);
+                float u = u0 * (1 - w) + u1 * w;
+                float v = v0 * (1 - w) + v1 * w;
+                // Trazi boju u teksturi koristeći interpolirane UV koordinate
+                TGAColor color = texture.get(u, v);
+                // Postavlja boju pixela na slici
+                image.set(i, j, color);
+            }
+        }
+    }
+}
+
+void draw_triangle_tex_corrected(float x0, float y0, float z0, float u0, float v0,
+                                 float x1, float y1, float z1, float u1, float v1,
+                                 float x2, float y2, float z2, float u2, float v2,
+                                 const char* file_path, TGAImage& image) {
+    // Učitava teksturu s file patha
+    TGAImage texture;
+    texture.read_tga_file(file_path);
+
+    Vec3 a = {x0, y0, z0};
+    Vec3 b = {x1, y1, z1};
+    Vec3 c = {x2, y2, z2};
+    Vec2 uv_a = {u0, v0};
+    Vec2 uv_b = {u1, v1};
+    Vec2 uv_c = {u2, v2};
+
+    // Dijeli koordinate sa z
+    a[0] /= a[2];
+    a[1] /= a[2];
+    b[0] /= b[2];
+    b[1] /= b[2];
+    c[0] /= c[2];
+    c[1] /= c[2];
+
+    // transformacija koordinata
+    a[0] = (1 + a[0]) * 0.5 * width;
+    a[1] = (1 + a[1]) * 0.5 * height;
+    b[0] = (1 + b[0]) * 0.5 * width;
+    b[1] = (1 + b[1]) * 0.5 * height;
+    c[0] = (1 + c[0]) * 0.5 * width;
+    c[1] = (1 + c[1]) * 0.5 * height;
+
+    // inverz 1/z
+    a[2] = 1 / a[2];
+    b[2] = 1 / b[2];
+    c[2] = 1 / c[2];
+
+    for (int j = 0; j < height; j++) {
+        for (int i = 0; i < width; i++) {
+            Vec3 p = {(float) i + 0.5f, height - (float) j + 0.5f, 0};
+            float alpha = rubnafunk(c, b, p);
+            float beta = rubnafunk(a, c, p);
+            float gama = rubnafunk(b, a, p);
+            if (alpha >= 0 && beta >= 0 && gama >= 0) {
+                // Interpolira UV koordinate pomoću baricentričnih koordinata
+                float w = alpha / (alpha + beta + gama);
+                float u = u0 * (1 - w) + u1 * w;
+                float v = v0 * (1 - w) + v1 * w;
+                // Trazi boju u teksturi koristeći interpolirane UV koordinate
+                TGAColor color = texture.get(u, v);
+                // Postavlja boju pixela na slici
+                image.set(i, j, color);
+            }
+        }
+    }
+}
+
+int main()
+{
+    // definiraj sliku
+    TGAImage image(width, height, TGAImage::RGB);
+
+    // nacrtaj trokute
+    draw_triangle(-48, -10,  82, 29, -15,  44, 13,  34, 114,image, blue);
+    //raw_triangle_tex(-48, -10,  82, 29, -15,  44, 13,  34, 114,0,0,0,1,1,0,image,"text.tga");
+    //draw_triangle_tex_corrected(-48, -10,  82, 29, -15,  44, 13,  34, 114,0,0,0,1,1,0,image,"text.tga");
+    //raw_triangle_tex(-48, -10,  82, 29, -15,  44, 13,  34, 114,0,0,0,1,1,0,image,"image1.tga");
+    //draw_triangle_tex_corrected(-48, -10,  82, 29, -15,  44, 13,  34, 114,0,0,0,1,1,0,image,"image2.tga");
+    // spremi sliku
+    image.flip_vertically();
+    image.write_tga_file("triangle.tga");
+}
